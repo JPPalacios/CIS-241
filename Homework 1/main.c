988eb997d4bd08d51a097b7a79e24c50c258d6b0
JPPalacios <56488109+JPPalacios@users.noreply.github.com>
2022-05-15 15:03:09 -0400

    Updated header block

--- Homework 1/main.c
+++ Homework 1/main.c
@@ -1,13 +1,22 @@
-/* 
+/************************************************************ 
+* FILENAME: main.c  
 *
+* ENGINEER: Juan Paulo Palacios 
 *
-*/
+* INSTRUCTOR: Professor Bhuse
+* 
+* DESCRIPTION:
+*       Heart rate is an important factor to consider during intensive
+*       physical activities. This program prompts a user's age to then 
+*       calculate their maximum heart rate and target heart rates
+* VERSION: 
+************************************************************/
 
 #include <stdio.h>
 
 int main()
 {
-    
+    printf("Testing 123!");
 
     return 0;
 }
